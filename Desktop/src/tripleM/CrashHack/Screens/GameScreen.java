4e86162a6dab0abb1b941515bd8a3eb80ee62e83
Raúl Marín <Algunenano@gmail.com>
2012-04-09 03:37:53 +0200

    Added new (better) font + Show example test

--- Desktop/src/tripleM/CrashHack/Screens/GameScreen.java
+++ Desktop/src/tripleM/CrashHack/Screens/GameScreen.java
@@ -4,11 +4,14 @@ import tripleM.CrashHack.General.Control;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen {
 	private SpriteBatch spriteBatch;
 	private Control control;
+	private int map;
+	private BitmapFont font;
 
 	
 	public GameScreen(Control _c) {
@@ -16,18 +19,19 @@ public class GameScreen implements Screen {
 		control = _c;
 		control.loadArt();
 		control.resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		//font = new BitmapFont(Gdx.files.internal("res/fonts/droidSansMono_32.fnt"), false);
+		font = new BitmapFont(Gdx.files.internal("res/fonts/droidSans_Border.fnt"), false);
 	}
 	
 	@Override
 	public void dispose() {
 		spriteBatch.dispose();
+		font.dispose();
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -37,8 +41,11 @@ public class GameScreen implements Screen {
 	}
 
 	@Override
-	public void render(float arg0) {
-		control.render(arg0);
+	public void render(float _delta) {
+		control.render(_delta);
+		spriteBatch.begin();
+		font.draw(spriteBatch, "Wooolololo", 100, 100);
+		spriteBatch.end();
 	}
 
 	@Override
